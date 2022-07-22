c4043de53986fb055ae0caf3fadb70da09af970b
dodic <dodickgod@gmail.com>
2022-07-22 22:09:57 +0300

    Fix unsafe in `FuntyPart`

--- rust/src/link_type.rs
+++ rust/src/link_type.rs
@@ -1,28 +1,23 @@
 use funty::Unsigned;
-use std::convert::TryFrom;
+use std::convert::{Infallible, TryFrom};
 use std::hint;
 use std::marker::Destruct;
-use std::ops::Div;
 
-/// # Safety
-/// By default implementation this trait can be used only with [`Unsigned`] types.
-///
-/// [`Unsigned`]: funty::Unsigned
-pub unsafe trait FuntyPart: Sized + TryFrom<u8> {
+pub trait FuntyPart: Sized + TryFrom<u8> {
     fn funty(n: u8) -> Self;
 }
 
-// SAFETY: later it will impl only for `Unsigned` (u8..u128)
-unsafe impl<All: TryFrom<u8>> const FuntyPart for All {
+// TryFrom<u8> has `Error = Infallible` for all types
+impl<All: TryFrom<u8, Error = Infallible>> const FuntyPart for All {
     fn funty(n: u8) -> Self
     where
         All: ~const Destruct,
-        <All as TryFrom<u8>>::Error: ~const Destruct,
     {
+        // std Result::unwrap_unchecked is not const
         match All::try_from(n) {
             Ok(all) => all,
             Err(_) => {
-                // SAFETY: T is unsigned and u8 is always less than T::MAX.
+                // <All as TryFrom<u8>>::Error is Infallible
                 unsafe { hint::unreachable_unchecked() }
             }
         }
@@ -31,4 +26,4 @@ unsafe impl<All: TryFrom<u8>> const FuntyPart for All {
 
 pub trait LinkType: Unsigned + FuntyPart {}
 
-impl<All: Unsigned + FuntyPart> const LinkType for All where All: ~const Div {}
+impl<All: Unsigned + FuntyPart> const LinkType for All {}
