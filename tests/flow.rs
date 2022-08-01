3e1f5a3d52d7afabaaa4ba44679932a922d282bf
dodic <dodickgod@gmail.com>
2022-08-01 12:45:26 +0300

    Fix fmt

--- tests/flow.rs
+++ tests/flow.rs
@@ -6,11 +6,7 @@ fn basic() {
 
     (0..20).try_for_each(|i| {
         vec.push(i);
-        if i == 10 {
-            Flow::Break
-        } else {
-            Flow::Continue
-        }
+        if i == 10 { Flow::Break } else { Flow::Continue }
     });
 
     assert_eq!(vec, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
